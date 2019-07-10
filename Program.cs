a86c43065f068cb65462a857791ae9e145d470f2
Parneet Kaur <742279@student.tor.loc>
2019-07-10 08:34:00 -0400

    Assignment 3 first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assign_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class TextProcessing
+    {
+        public void Run()
+        {
+            //open the file and access the data
+        }
+    }
+}
